6d9113b2010319973f6c35d9aeea6a45b76ec0da
Kris1027 <obarzanek.work@gmail.com>
2024-02-06 13:45:01 +0100

    build first section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import Image from "next/image";
+import GithubImg from "../public/github.png";
+
 import { BsMoonStarsFill } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -20,6 +26,29 @@ export default function Home() {
             </li>
           </ul>
         </nav>
+        <div className="text-center p-10">
+          <h2 className="text-5xl py-2 text-teal-600 font-medium">
+            Krzysztof Obarzanek
+          </h2>
+          <h3 className="text-2xl py-2">Frontend developer</h3>
+          <p className="text-md py-5 leading-8 text-gray-800">
+            Freelancer providing services for programming and design content
+            needs. Join me down below and let&apos;s get cracking!
+          </p>
+        </div>
+        <div className="text-5xl flex justify-center gap-16 text-gray-600">
+          <FaLinkedin className="cursor-pointer" />
+          <FaGithub className="cursor-pointer" />
+          <FaInstagramSquare className="cursor-pointer" />
+        </div>
+        <div className="relative rounded-full w-80 h-80 mx-auto mt-20 overflow-hidden">
+          <Image
+            src={GithubImg}
+            alt="image from github"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
       </section>
     </main>
   );
