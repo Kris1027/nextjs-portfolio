182d8b31c3efab3c920f663cbe525a0b7f43b1a8
Kris1027 <obarzanek.work@gmail.com>
2024-02-13 23:43:15 +0100

    build splitStringUsingRegex  and use in About component

---
+++ app/components/util/splitStringUsingRegex.tsx
@@ -0,0 +1,12 @@
+export function splitStringUsingRegex(inputString: string): string[] {
+  const characters: string[] = [];
+  const regex = /[\s\S]/gu;
+
+  let match;
+
+  while ((match = regex.exec(inputString)) !== null) {
+    characters.push(match[0]);
+  }
+
+  return characters;
+}
--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { splitStringUsingRegex } from "../components/util/splitStringUsingRegex";
 
 import GithubImg from "../../public/github.png";
-import Heading from "../components/heading";
 
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -9,18 +10,70 @@ import { FaInstagramSquare } from "react-icons/fa";
 
 const descripion =
   "Since November 2022, I've been diving into frontend development, fueled by a passion for creating captivating digital experiences. From mastering HTML, CSS, and JavaScript to exploring advanced technologies like React and TypeScript, I'm dedicated to continuous learning. Despite challenges, I persistently push forward, always seeking new techniques and best practices. I prioritize design principles and usability, striving to strike the perfect balance between form and function. As I continue my journey, I'm excited to collaborate, contribute, and grow in this dynamic field.";
+const title = "Krzysztof Obarzanek";
+const profession = "Frontend developer";
+
+const charVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 export default function About() {
+  const titleChars = splitStringUsingRegex(title);
+  const professionChars = splitStringUsingRegex(profession);
+  const descripionChars = splitStringUsingRegex(descripion);
+
   return (
     <section className="p-10 pt-40 bg-white dark:bg-black" id="about">
       <div>
-        <h1 className="text-5xl py-2 text-teal-600 font-bold text-center">
-          Krzysztof Obarzanek
-        </h1>
-        <Heading>Frontend developer</Heading>
-        <p className="text-sm md:text-lg pb-5 leading-8 text-gray-800 dark:text-gray-600 lg:w-1/2 mx-auto text-center">
-          {descripion}
-        </p>
+        <motion.h1
+          initial="hidden"
+          whileInView="visible"
+          transition={{ staggerChildren: 0.02 }}
+          className="text-5xl py-2 text-teal-600 font-bold text-center"
+        >
+          {titleChars.map((char) => (
+            <motion.span
+              key={char}
+              variants={charVariants}
+              transition={{ duration: 0.1 }}
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.h1>
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          transition={{ staggerChildren: 0.2 }}
+          className="text-2xl pt-2 dark:text-gray-500 pb-10 text-center"
+        >
+          {professionChars.map((char) => (
+            <motion.span
+              key={char}
+              variants={charVariants}
+              transition={{ duration: 0.3 }}
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.h2>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          transition={{ staggerChildren: 0.006 }}
+          className="text-sm md:text-lg pb-5 leading-8 text-gray-800 dark:text-gray-600 lg:w-1/2 mx-auto text-center"
+        >
+          {descripionChars.map((char) => (
+            <motion.span
+              key={char}
+              variants={charVariants}
+              transition={{ duration: 0.1 }}
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.p>
       </div>
       <div className="text-5xl flex justify-center gap-16 text-gray-600">
         <a
