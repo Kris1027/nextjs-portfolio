a2c9924b19c9654acb9ca4e90827e27c31733b5d
Kris1027 <obarzanek.work@gmail.com>
2024-02-15 12:25:24 +0100

    refactor skills icons arrays

--- app/sections/skills.tsx
+++ app/sections/skills.tsx
@@ -9,6 +9,7 @@ import { DiMongodb } from "react-icons/di";
 import { SiTailwindcss } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { SiVite } from "react-icons/si";
+import { SiAstro } from "react-icons/si";
 
 import { AiOutlineConsoleSql } from "react-icons/ai";
 import { SiMysql } from "react-icons/si";
@@ -19,38 +20,117 @@ import { FaVuejs } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
 
 export default function Skills() {
+  const skillsIcons = [
+    {
+      icon: <FaHtml5 />,
+      color: "orange-500",
+    },
+    {
+      icon: <FaCss3Alt />,
+      color: "blue-500",
+    },
+    {
+      icon: <IoLogoJavascript />,
+      color: "yellow-500",
+    },
+    {
+      icon: <FaReact />,
+      color: "blue-600",
+    },
+    {
+      icon: <SiTypescript />,
+      color: "blue-500",
+    },
+    {
+      icon: <FaNodeJs />,
+      color: "green-500",
+    },
+    {
+      icon: <SiExpress />,
+      color: "amber-500",
+    },
+    {
+      icon: <DiMongodb />,
+      color: "green-950",
+    },
+    {
+      icon: <SiTailwindcss />,
+      color: "cyan-500",
+    },
+    {
+      icon: <TbBrandNextjs />,
+      color: "slate-400",
+    },
+    {
+      icon: <SiVite />,
+      color: "violet-600",
+    },
+    {
+      icon: <SiAstro />,
+      color: "red-500",
+    },
+  ];
+
+  const nextToLearn = [
+    {
+      icon: <AiOutlineConsoleSql />,
+      color: "blue-500",
+    },
+    {
+      icon: <SiMysql />,
+      color: "blue-500",
+    },
+    {
+      icon: <SiCypress />,
+      color: "green-500",
+    },
+    {
+      icon: <FaMobileAlt />,
+      secondIcon: <FaReact />,
+      color: "blue-600",
+    },
+    {
+      icon: <FaAngular />,
+      color: "red-500",
+    },
+    {
+      icon: <FaVuejs />,
+      color: "green-500",
+    },
+    {
+      icon: <FaPython />,
+      color: "yellow-500",
+    },
+  ];
+
   return (
     <section className="p-10 pt-40 bg-white dark:bg-black" id="skills">
       <h2 className="text-2xl pt-2 dark:text-gray-500 pb-10 text-center">
         Know technologies
       </h2>
       <div className="text-5xl flex flex-wrap justify-center gap-16 text-gray-600 pb-10">
-        <FaHtml5 className="hover:text-orange-500 hover:scale-110" />
-        <FaCss3Alt className="hover:text-blue-500 hover:scale-110" />
-        <IoLogoJavascript className="hover:text-yellow-500 hover:scale-110" />
-        <FaReact className="hover:text-blue-600 hover:scale-110" />
-        <SiTypescript className="hover:text-blue-500 hover:scale-110" />
-        <FaNodeJs className="hover:text-green-500 hover:scale-110" />
-        <SiExpress className="hover:text-amber-500 hover:scale-110" />
-        <DiMongodb className="hover:text-green-950 hover:scale-110" />
-        <SiTailwindcss className="hover:text-cyan-500 hover:scale-110" />
-        <TbBrandNextjs className="hover:text-slate-400 hover:scale-110" />
-        <SiVite className="hover:text-violet-600 hover:scale-110" />
+        {skillsIcons.map((icon, index) => (
+          <div
+            key={index}
+            className={`hover:text-${icon.color} hover:scale-110`}
+          >
+            {icon.icon}
+          </div>
+        ))}
       </div>
       <h3 className="text-2xl pt-2 dark:text-gray-500 pb-10 text-center">
         Next to learn
       </h3>
       <div className="text-5xl flex flex-wrap justify-center gap-16 text-gray-600 pb-10">
-        <AiOutlineConsoleSql className="hover:text-blue-500 hover:scale-110" />
-        <SiMysql className="hover:text-blue-500 hover:scale-110" />
-        <SiCypress className="hover:text-green-500 hover:scale-110" />
-        <div className="flex hover:text-blue-600 hover:scale-110">
-          <FaMobileAlt />
-          <FaReact />
-        </div>
-        <FaAngular className="hover:text-red-500 hover:scale-110" />
-        <FaVuejs className="hover:text-green-500 hover:scale-110" />
-        <FaPython className="hover:text-yellow-500 hover:scale-110" />
+        {nextToLearn.map((icon, index) => (
+          <div
+            key={index}
+            className={`flex hover:text-${icon.color} hover:scale-110`}
+          >
+            {icon.icon}
+            {icon.secondIcon && icon.secondIcon}
+          </div>
+        ))}
       </div>
     </section>
   );
