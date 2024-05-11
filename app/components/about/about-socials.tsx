51fe53ac1f96583f564bad272151b4ce96255061
Kris1027 <obarzanek.work@gmail.com>
2024-05-11 14:08:51 +0200

    create socials in data

--- app/components/about/about-socials.tsx
+++ app/components/about/about-socials.tsx
@@ -1,25 +1,7 @@
 "use client";
-import { FaGithub, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
+import { socials } from "@/app/data/socials";
 import { fadeInAnimationVariants } from "../skill-item";
 import { motion } from "framer-motion";
-
-const socials = [
-  {
-    icon: <FaLinkedin />,
-    link: "https://www.linkedin.com/in/krzysztof-obarzanek-6b8803254/",
-    color: "#3b82f6",
-  },
-  {
-    icon: <FaGithub />,
-    link: "https://github.com/Kris1027",
-    color: "#111827",
-  },
-  {
-    icon: <FaInstagramSquare />,
-    link: "https://www.instagram.com/krzy1027/",
-    color: "#ef4444",
-  },
-];
 
 export default function AboutSocials() {
   return (
