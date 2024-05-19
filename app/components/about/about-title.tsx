b8460d83d111364c43097e2a440299ab26bd5209
Kris1027 <obarzanek.work@gmail.com>
2024-05-19 10:40:20 +0200

    read about data from database

--- app/components/about/about-title.tsx
+++ app/components/about/about-title.tsx
@@ -1,23 +1,20 @@
-export default function AboutTitle() {
+import db from "@/db/db";
+
+export default async function AboutTitle() {
+  const title = await db.aboutTitle.findFirstOrThrow();
+  const subtitle = await db.aboutSubtitle.findFirstOrThrow();
+  const description = await db.aboutDescription.findFirstOrThrow();
+
   return (
     <>
       <h1 className="text-5xl tablet:text-7xl desktop:text-9xl py-2 text-primary dark:text-primaryDark font-bold text-center">
-        Krzysztof Obarzanek
+        {title?.title}
       </h1>
       <h2 className="text-2xl tablet:text-4xl desktop:text-6xl pt-2 text-secondary dark:text-secondaryDark pb-10 text-center">
-        Frontend developer
+        {subtitle?.title}
       </h2>
       <p className="text-sm tablet:text-lg desktop:text-xl pb-5 leading-8 text-secondary dark:text-secondaryDark text-center tablet:w-4/5 desktop:w-3/5 mx-auto">
-        Hi there! I am a web developer who is passionate about creating
-        user-friendly and visually appealing websites. I have been learning to
-        code since November 2022, and I have experience with HTML, CSS,
-        JavaScript, React, and TypeScript. I am always eager to learn new
-        technologies and improve my skills. I believe that web development is a
-        powerfull tool that can be used to create positive change in the world.
-        I am excited to use my skills to build websites that make a difference.
-        I am currently looking for a job as a frontend developer, so if you
-        think I would be a good fit for your team, please don&apos;t hesitate to
-        reach out. Thank you for your time!
+        {description?.title}
       </p>
     </>
   );
