aac2206d202a417433bbd10ba2bf33828c6b0be0
Kris1027 <obarzanek.work@gmail.com>
2024-05-20 00:20:03 +0200

    update screen

--- app/ui/list.tsx
+++ app/ui/list.tsx
@@ -1,6 +1,6 @@
 export default function List({ children }: { children: React.ReactNode }) {
   return (
-    <ul className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-8">
+    <ul className="grid grid-cols-1 mobile:grid-cols-2 desktop:grid-cols-3 gap-8">
       {children}
     </ul>
   );
