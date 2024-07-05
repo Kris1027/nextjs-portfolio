eb09202d56340093103c5adf3fb20c8be9b6fced
Kris1027 <obarzanek.work@gmail.com>
2024-07-05 11:23:23 +0200

    fix navigation links test

--- tests/navigationLinks.spec.ts
+++ tests/navigationLinks.spec.ts
@@ -1,10 +1,20 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("Navigation links", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await page.getByRole("link", { name: "About" }).click();
-  await page.getByRole("link", { name: "Skills" }).click();
-  await page.getByRole("link", { name: "Projects" }).click();
-  await page.getByRole("link", { name: "Courses" }).click();
-  await page.getByRole("link", { name: "Contact" }).click();
+
+  await page.getByLabel("About").click();
+  await expect(page).toHaveURL("http://localhost:3000/#about");
+
+  await page.getByLabel("Skills").click();
+  await expect(page).toHaveURL("http://localhost:3000/#skills");
+
+  await page.getByLabel("Projects").click();
+  await expect(page).toHaveURL("http://localhost:3000/#projects");
+
+  await page.getByLabel("Courses").click();
+  await expect(page).toHaveURL("http://localhost:3000/#courses");
+
+  await page.getByLabel("Contact").click();
+  await expect(page).toHaveURL("http://localhost:3000/#contact");
 });
