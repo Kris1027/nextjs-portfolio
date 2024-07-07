329e2b201ee8e3a63e56b65a95a97af609e43ff8
Kris1027 <obarzanek.work@gmail.com>
2024-07-07 11:49:04 +0200

    fix about label names

--- app/components/about/about-title.tsx
+++ app/components/about/about-title.tsx
@@ -10,7 +10,7 @@ export default async function AboutTitle() {
     <>
       <Reveal>
         <h1
-          aria-label="title"
+          aria-label="about title"
           className="text-5xl tablet:text-7xl desktop:text-9xl text-[var(--primary)] font-bold py-2 mobile:py-4 text-center"
         >
           {title?.title}
@@ -18,7 +18,7 @@ export default async function AboutTitle() {
       </Reveal>
       <Reveal>
         <h2
-          aria-label="proffesion"
+          aria-label="about proffesion"
           className="text-2xl tablet:text-4xl desktop:text-6xl text-center"
         >
           {subtitle?.title}
@@ -26,7 +26,7 @@ export default async function AboutTitle() {
       </Reveal>
       <Reveal>
         <p
-          aria-label="description"
+          aria-label="about description"
           className="text-sm text-[var(--secondary)] tablet:text-lg desktop:text-xl leading-8 tablet:w-4/5 desktop:w-3/5 mx-auto py-2 tablet:py-5 text-center"
         >
           {description?.title}
