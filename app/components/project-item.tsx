8bef0b609c152e8bc0ee8a2d5435cccbb8cc3c87
Kris1027 <obarzanek.work@gmail.com>
2024-08-24 11:47:38 +0200

    add in progress with own style

--- app/components/project-item.tsx
+++ app/components/project-item.tsx
@@ -1,82 +1,86 @@
-"use client";
-import Image from "next/image";
-import { GoLinkExternal } from "react-icons/go";
-import { imageKitLoader } from "../util/image-kit-loader";
-import ListItem from "../ui/list-item";
-import { sideAnimation } from "../theme/animations/side-animation";
+'use client';
+import Image from 'next/image';
+import { GoLinkExternal } from 'react-icons/go';
+import { imageKitLoader } from '../util/image-kit-loader';
+import ListItem from '../ui/list-item';
+import { sideAnimation } from '../theme/animations/side-animation';
 
 type ProjectProps = {
-  id: number;
-  title: string;
-  image: string;
-  live: string;
-  github: string;
-  technologies: string[];
-  description: string[];
+   id: number;
+   title: string;
+   image: string;
+   live: string;
+   github: string;
+   technologies: string[];
+   description: string[];
 };
 
 export default function ProjectItem({
-  id,
-  title,
-  image,
-  live,
-  github,
-  technologies,
-  description,
+   id,
+   title,
+   image,
+   live,
+   github,
+   technologies,
+   description,
 }: ProjectProps) {
-  return (
-    <ListItem id={id} variants={sideAnimation}>
-      <div className="relative bg-[var(--background)] rounded-t-lg overflow-hidden">
-        <Image
-          className="scale-[1.4] rotate-12"
-          loader={imageKitLoader}
-          src={image}
-          sizes="(min-width: 768px) 564px, (min-width: 384px) 224px"
-          alt={title}
-          width={564}
-          height={233}
-        />
-      </div>
-      <div className="flex flex-col justify-between h-full">
-        <ul className="flex items-center flex-wrap gap-1">
-          {technologies.map((tech, index) => {
-            return (
-              <li
-                className="border-[1px] border-[var(--secondary)] rounded-2xl py-1 px-5 hover:text-[var(--primary)] hover:border-[var(--primary)] text-xs tablet:text-xs"
-                key={index}
-              >
-                {tech}
-              </li>
-            );
-          })}
-        </ul>
-        <h3 className="text-xl tablet:text-3xl">{title}</h3>
-        <ul className="list-disc pl-4 space-y-2 text-xs tablet:text-sm">
-          {description.map((desc, index) => {
-            return <li key={index}>{desc}</li>;
-          })}
-        </ul>
-        <div className="flex justify-between text-xs tablet:text-base">
-          <a
-            className="flex items-center gap-2 border-[1px] border-[var(--secondary)] py-1 px-3 cursor-pointer hover:text-[var(--link)] hover:border-[var(--link)]"
-            href={live}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Check Live
-            <GoLinkExternal />
-          </a>
-          <a
-            className="flex items-center gap-2 border-[1px] border-[var(--secondary)] py-1 px-3 cursor-pointer hover:text-[var(--link)] hover:border-[var(--link)]"
-            href={github}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See Code
-            <GoLinkExternal />
-          </a>
-        </div>
-      </div>
-    </ListItem>
-  );
+   return (
+      <ListItem id={id} variants={sideAnimation}>
+         <div className='relative bg-[var(--background)] rounded-t-lg overflow-hidden'>
+            <Image
+               className='scale-[1.4] rotate-12'
+               loader={imageKitLoader}
+               src={image}
+               sizes='(min-width: 768px) 564px, (min-width: 384px) 224px'
+               alt={title}
+               width={564}
+               height={233}
+            />
+         </div>
+         <div className='flex flex-col justify-between h-full'>
+            <ul className='flex items-center justify-center flex-wrap gap-1'>
+               {technologies.map((tech, index) => {
+                  return (
+                     <li
+                        className={`border-[1px] border-[var(--secondary)] rounded-2xl py-1 px-5 hover:text-[var(--primary)] hover:border-[var(--primary)] text-xs tablet:text-xs ${
+                           tech === 'In progress...'
+                              ? 'text-[var(--error)] scale-150'
+                              : ''
+                        }`}
+                        key={index}
+                     >
+                        {tech}
+                     </li>
+                  );
+               })}
+            </ul>
+            <h3 className='text-xl tablet:text-3xl'>{title}</h3>
+            <ul className='list-disc pl-4 space-y-2 text-xs tablet:text-sm'>
+               {description.map((desc, index) => {
+                  return <li key={index}>{desc}</li>;
+               })}
+            </ul>
+            <div className='flex justify-between text-xs tablet:text-base'>
+               <a
+                  className='flex items-center gap-2 border-[1px] border-[var(--secondary)] py-1 px-3 cursor-pointer hover:text-[var(--link)] hover:border-[var(--link)]'
+                  href={live}
+                  target='_blank'
+                  rel='noopener noreferrer'
+               >
+                  Check Live
+                  <GoLinkExternal />
+               </a>
+               <a
+                  className='flex items-center gap-2 border-[1px] border-[var(--secondary)] py-1 px-3 cursor-pointer hover:text-[var(--link)] hover:border-[var(--link)]'
+                  href={github}
+                  target='_blank'
+                  rel='noopener noreferrer'
+               >
+                  See Code
+                  <GoLinkExternal />
+               </a>
+            </div>
+         </div>
+      </ListItem>
+   );
 }
--- app/views/projects.tsx
+++ app/views/projects.tsx
@@ -1,53 +1,53 @@
-import db from "@/db/db";
-import ProjectItem from "../components/project-item";
-import Section from "../ui/section";
-import Heading from "../ui/heading";
-import Paragraph from "../ui/paragraph";
-import List from "../ui/list";
-import Reveal from "../theme/animations/reveal";
+import db from '@/db/db';
+import ProjectItem from '../components/project-item';
+import Section from '../ui/section';
+import Heading from '../ui/heading';
+import Paragraph from '../ui/paragraph';
+import List from '../ui/list';
+import Reveal from '../theme/animations/reveal';
 
 export default async function Projects() {
-  const projects = await db.projects.findMany({
-    include: {
-      technologies: true,
-      description: true,
-    },
-  });
+   const projects = await db.projects.findMany({
+      include: {
+         technologies: true,
+         description: true,
+      },
+   });
 
-  return (
-    <Section id="projects">
-      <Heading ariaLabel="projects heading">Projects</Heading>
-      <Paragraph ariaLabel="projects description">
-        Here are some of the projects I&apos;ve worked on
-      </Paragraph>
-      <List dataTestId="projects list">
-        {projects.map((project) => (
-          <ProjectItem
-            key={project.id}
-            id={project.id}
-            title={project.title}
-            image={project.image}
-            live={project.live}
-            github={project.github}
-            technologies={project.technologies.map((tech) => tech.name)}
-            description={project.description.map((desc) => desc.name)}
-          />
-        ))}
-      </List>
-      <Reveal>
-        <Paragraph ariaLabel="projects github link">
-          More you can find on my{" "}
-          <a
-            className="text-[var(--primary)] hover:underline font-bold"
-            href="https://github.com/Kris1027"
-            target="_blank"
-            rel="norefferer"
-          >
-            GitHub
-          </a>{" "}
-          profile!
-        </Paragraph>
-      </Reveal>
-    </Section>
-  );
+   return (
+      <Section id='projects'>
+         <Heading ariaLabel='projects heading'>Projects</Heading>
+         <Paragraph ariaLabel='projects description'>
+            Here are some of the projects I&apos;ve worked on
+         </Paragraph>
+         <List dataTestId='projects list'>
+            {projects.map((project) => (
+               <ProjectItem
+                  key={project.id}
+                  id={project.id}
+                  title={project.title}
+                  image={project.image}
+                  live={project.live}
+                  github={project.github}
+                  technologies={project.technologies.map((tech) => tech.name)}
+                  description={project.description.map((desc) => desc.name)}
+               />
+            ))}
+         </List>
+         <Reveal>
+            <Paragraph ariaLabel='projects github link'>
+               More you can find on my{' '}
+               <a
+                  className='text-[var(--primary)] hover:underline font-bold'
+                  href='https://github.com/Kris1027'
+                  target='_blank'
+                  rel='norefferer'
+               >
+                  GitHub
+               </a>{' '}
+               profile!
+            </Paragraph>
+         </Reveal>
+      </Section>
+   );
 }
