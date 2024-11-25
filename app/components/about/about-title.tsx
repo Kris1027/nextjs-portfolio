a9ca70f4d3056e9f64ce09bf37d64e6dbdab6e79
Kris1027 <obarzanek.work@gmail.com>
2024-11-25 14:49:03 +0100

    full width for description and smaller name and title text

--- app/components/about/about-title.tsx
+++ app/components/about/about-title.tsx
@@ -1,37 +1,34 @@
-import Reveal from "@/app/theme/animations/reveal";
-import db from "@/db/db";
+import Reveal from '@/app/theme/animations/reveal';
+import db from '@/db/db';
 
 export default async function AboutTitle() {
-  const title = await db.aboutTitle.findFirstOrThrow();
-  const subtitle = await db.aboutSubtitle.findFirstOrThrow();
-  const description = await db.aboutDescription.findFirstOrThrow();
+    const title = await db.aboutTitle.findFirstOrThrow();
+    const subtitle = await db.aboutSubtitle.findFirstOrThrow();
+    const description = await db.aboutDescription.findFirstOrThrow();
 
-  return (
-    <>
-      <Reveal>
-        <h1
-          aria-label="about title"
-          className="text-5xl tablet:text-7xl desktop:text-9xl text-[var(--primary)] font-bold py-2 mobile:py-4 text-center"
-        >
-          {title?.title}
-        </h1>
-      </Reveal>
-      <Reveal>
-        <h2
-          aria-label="about proffesion"
-          className="text-2xl tablet:text-4xl desktop:text-6xl text-center"
-        >
-          {subtitle?.title}
-        </h2>
-      </Reveal>
-      <Reveal>
-        <p
-          aria-label="about description"
-          className="text-sm text-[var(--secondary)] tablet:text-lg desktop:text-xl leading-8 tablet:w-4/5 desktop:w-3/5 mx-auto py-2 tablet:py-5 text-center"
-        >
-          {description?.title}
-        </p>
-      </Reveal>
-    </>
-  );
+    return (
+        <>
+            <Reveal>
+                <h1
+                    aria-label='about title'
+                    className='text-5xl tablet:text-6xl text-[var(--primary)] font-bold py-2 mobile:py-4 text-center'
+                >
+                    {title?.title}
+                </h1>
+            </Reveal>
+            <Reveal>
+                <h2 aria-label='about proffesion' className='text-2xl tablet:text-4xl text-center'>
+                    {subtitle?.title}
+                </h2>
+            </Reveal>
+            <Reveal>
+                <p
+                    aria-label='about description'
+                    className='text-sm text-[var(--secondary)] tablet:text-lg desktop:text-xl leading-8mx-auto py-2 tablet:py-5 text-center'
+                >
+                    {description?.title}
+                </p>
+            </Reveal>
+        </>
+    );
 }
