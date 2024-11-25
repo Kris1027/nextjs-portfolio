ad19290b91b3f326e3d1dd5621949127590e1a16
Kris1027 <obarzanek.work@gmail.com>
2024-11-25 16:14:43 +0100

    update database and create animate background

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import About from "./views/about";
-import Projects from "./views/projects";
-import Skills from "./views/skills";
-import Courses from "./views/courses";
-import Contact from "./views/contact";
+import About from './views/about';
+import Projects from './views/projects';
+import Skills from './views/skills';
+import Courses from './views/courses';
+import Contact from './views/contact';
 
 export default function Home() {
-  return (
-    <main>
-      <About />
-      <Skills />
-      <Projects />
-      <Courses />
-      <Contact />
-      <ToastContainer />
-    </main>
-  );
+    return (
+        <main>
+            <About />
+            <Skills />
+            <Projects />
+            <Courses />
+            <Contact />
+            <ToastContainer />
+        </main>
+    );
 }
