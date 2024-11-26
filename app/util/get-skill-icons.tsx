b7034792ab753f5ecb5118069dba0694a9e472bf
Kris1027 <obarzanek.work@gmail.com>
2024-11-26 07:42:51 +0100

    update db

--- app/util/get-skill-icons.tsx
+++ app/util/get-skill-icons.tsx
@@ -1,74 +1,75 @@
 import { BiLogoPostgresql } from 'react-icons/bi';
 import { DiMongodb } from 'react-icons/di';
+import { FaCss3Alt, FaGitAlt, FaHtml5, FaNodeJs, FaReact, FaSass } from 'react-icons/fa';
+import { IoLogoJavascript, IoCloudDoneOutline } from 'react-icons/io5';
 import {
-   FaCss3Alt,
-   FaGitAlt,
-   FaHtml5,
-   FaNodeJs,
-   FaReact,
-   FaSass,
-} from 'react-icons/fa';
-import { IoLogoJavascript } from 'react-icons/io5';
-import {
-   SiAstro,
-   SiExpress,
-   SiMysql,
-   SiPlaywright,
-   SiPrisma,
-   SiStyledcomponents,
-   SiTailwindcss,
-   SiTypescript,
-   SiJest,
-   SiFramer,
-   SiSupabase,
+    SiAstro,
+    SiExpress,
+    SiMysql,
+    SiPlaywright,
+    SiPrisma,
+    SiStyledcomponents,
+    SiTailwindcss,
+    SiTypescript,
+    SiJest,
+    SiFramer,
+    SiSupabase,
+    SiAuth0,
+    SiJsonwebtokens,
 } from 'react-icons/si';
 import { TbBrandNextjs } from 'react-icons/tb';
 
 export const getSkillIcons = (iconName: string) => {
-   switch (iconName) {
-      case 'FaHtml5':
-         return <FaHtml5 />;
-      case 'FaCss3Alt':
-         return <FaCss3Alt />;
-      case 'IoLogoJavascript':
-         return <IoLogoJavascript />;
-      case 'FaReact':
-         return <FaReact />;
-      case 'SiTypescript':
-         return <SiTypescript />;
-      case 'FaNodeJs':
-         return <FaNodeJs />;
-      case 'SiExpress':
-         return <SiExpress />;
-      case 'DiMongodb':
-         return <DiMongodb />;
-      case 'BiLogoPostgresql':
-         return <BiLogoPostgresql />;
-      case 'SiMysql':
-         return <SiMysql />;
-      case 'SiTailwindcss':
-         return <SiTailwindcss />;
-      case 'FaSass':
-         return <FaSass />;
-      case 'SiStyledcomponents':
-         return <SiStyledcomponents />;
-      case 'TbBrandNextjs':
-         return <TbBrandNextjs />;
-      case 'FaGitAlt':
-         return <FaGitAlt />;
-      case 'SiAstro':
-         return <SiAstro />;
-      case 'SiPrisma':
-         return <SiPrisma />;
-      case 'SiPlaywright':
-         return <SiPlaywright />;
-      case 'SiJest':
-         return <SiJest />;
-      case 'SiFramer':
-         return <SiFramer />;
-      case 'SiSupabase':
-         return <SiSupabase />;
-      default:
-         return null;
-   }
+    switch (iconName) {
+        case 'FaHtml5':
+            return <FaHtml5 />;
+        case 'FaCss3Alt':
+            return <FaCss3Alt />;
+        case 'IoLogoJavascript':
+            return <IoLogoJavascript />;
+        case 'FaReact':
+            return <FaReact />;
+        case 'SiTypescript':
+            return <SiTypescript />;
+        case 'FaNodeJs':
+            return <FaNodeJs />;
+        case 'SiExpress':
+            return <SiExpress />;
+        case 'DiMongodb':
+            return <DiMongodb />;
+        case 'BiLogoPostgresql':
+            return <BiLogoPostgresql />;
+        case 'SiMysql':
+            return <SiMysql />;
+        case 'SiTailwindcss':
+            return <SiTailwindcss />;
+        case 'FaSass':
+            return <FaSass />;
+        case 'SiStyledcomponents':
+            return <SiStyledcomponents />;
+        case 'TbBrandNextjs':
+            return <TbBrandNextjs />;
+        case 'FaGitAlt':
+            return <FaGitAlt />;
+        case 'SiAstro':
+            return <SiAstro />;
+        case 'SiPrisma':
+            return <SiPrisma />;
+        case 'SiPlaywright':
+            return <SiPlaywright />;
+        case 'SiJest':
+            return <SiJest />;
+        case 'SiFramer':
+            return <SiFramer />;
+        case 'SiSupabase':
+            return <SiSupabase />;
+        case 'SiCloudinary':
+            return <IoCloudDoneOutline />;
+        case 'SiAuth0':
+            return <SiAuth0 />;
+        case 'SiJsonwebtokens':
+            return <SiJsonwebtokens />;
+        default:
+            return null;
+    }
 };
